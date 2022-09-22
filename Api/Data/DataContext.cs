3f64ad0f3bd74997ba232435847da1612e09aaec
Malek103 <malek-h2012@hotmail.com>
2022-09-22 14:28:02 +0300

    End of section 2

---
+++ Api/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+
+    }
+}
